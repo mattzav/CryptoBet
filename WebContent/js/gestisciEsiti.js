4da8021ad05f90b3f58a39f9ecc24d688a8b946d
mattzav <matteozav@hotmail.com>
2018-01-13 13:35:00 +0100

    aggiunto controllo sulla quota ed avvisi di errore

--- WebContent/js/gestisciEsiti.js
+++ WebContent/js/gestisciEsiti.js
@@ -24,24 +24,55 @@ function abilitaEsito(esito){
 	function aggiornaEsito(partita){
 		var esito=$("#inputModificaQuota"+partita+" "+'[name="'+"esitoScelto"+'"]').val();
 		var quota=$("#inputModificaQuota"+partita+" "+'[name="'+"nuovaQuota"+'"]').val();
-		
-		$.ajax({
-			  type: 'POST',
-			  url: 'modificaQuota',
-			  data: {
-				  esitoScelto:esito,
-				  nuovaQuota:quota,
-				  partita:partita
-			  },
-			  success: function(){
-				  $('[name="'+partita+";"+esito+'"]').val(quota);
+
+		var pattern_esito = /^(1|1X|12|X2|2|U|O|GG|NG)$/;
+		var pattern_quota= /^\d+(\.\d+)*$/;
+		if(pattern_esito.test(esito) && pattern_quota.test(quota)){
+			$("#alert"+partita+".alert").remove();
+			$(".partitaInModifica"+partita).parent().children().first().append(
+					"<div id=\"alert"+partita+"\" class=\"alert alert-success alert-dismissable\">"+
+					 "<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>"+
+					"<strong>Congratulazioni!</strong> hai aggiornato correttamente la tua quota"+
+					"</div>");
+			$.ajax({
+				type: 'POST',
+				url: 'modificaQuota',
+				data: {
+					esitoScelto:esito,
+					nuovaQuota:quota,
+					partita:partita
+				},
+				success: function(){
+					$('[name="'+partita+";"+esito+'"]').val(quota);
 				}
 			});
+		}
+		else{
+			var errore ="";
+			if(!pattern_esito.test(esito) && !pattern_quota.test(quota)){
+				errore+="hai inserito un esito non valido ed anche una quota non valida";
+			}
+			else if(!pattern_esito.test(esito)){
+				errore+="hai inserito un esito non valido";
+			}
+			else{
+				errore+="hai inserito una quota non valida";
+			}
+			$("#alert"+partita+".alert").remove();
+			$(".partitaInModifica"+partita).parent().children().first().append(
+					"<div id=\"alert"+partita+"\" class=\"alert alert-danger alert-dismissable\">"+
+					 "<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>"+
+					"<strong>Attenzione!</strong> "+errore+
+					"</div>");
+		}
 	}
 	
 	function selezionaPartitaDaModificare(partita,disabilita){
+		if($("#inputModificaQuota"+partita).length){
+			disabilita=true;
+		}
 		if(!disabilita){
-			$(".partitaInModifica"+partita).parent().children().first().append("<div id=\"inputModificaQuota"+partita+"\"> <div class=\"modal-body\"><div class=\"form-group\"><label for=\"esitoScelto\" class=\"form-control-label\">Esito da modificare:</label><input type=\"text\" class=\"form-control\"name=\"esitoScelto\"></div><div class=\"form-group\"><label for=\"nuovaQuota\" class=\"form-control-label\">Nuova Quota:</label><textarea class=\"form-control\" name=\"nuovaQuota\"></textarea></div></div><div class=\"modal-footer\"><button name=\"annulla\" onclick=\"selezionaPartitaDaModificare("+partita+",true)\" class=\"btn btn-secondary btn-sm\"data-dismiss=\"modal\">Annulla</button><button name=\"aggiorna\" onclick=\"aggiornaEsito("+partita+")\" class=\"btn btn-primary btn-sm\">Aggiorna Esito</button><button name=\"suggerimento\" onclick=\"richiediSuggerimento("+partita+")\" class=\"btn btn-info btn-sm\"> Richiedi Suggerimento</button></div></div>");
+			$(".partitaInModifica"+partita).parent().children().first().append("<div id=\"inputModificaQuota"+partita+"\"> <div class=\"modal-body\"><div class=\"form-group\"><label for=\"esitoScelto\" onkeyup=\"controllaEsito()\" class=\"form-control-label\">Esito da modificare:</label><input type=\"text\" class=\"form-control\"name=\"esitoScelto\"></div><div class=\"form-group\"><label for=\"nuovaQuota\" class=\"form-control-label\">Nuova Quota:</label><textarea class=\"form-control\" name=\"nuovaQuota\"></textarea></div></div><div class=\"modal-footer\"><button name=\"annulla\" onclick=\"selezionaPartitaDaModificare("+partita+",true)\" class=\"btn btn-secondary btn-sm\"data-dismiss=\"modal\">Annulla</button><button name=\"aggiorna\" onclick=\"aggiornaEsito("+partita+")\" class=\"btn btn-primary btn-sm\">Aggiorna Esito</button><button name=\"suggerimento\" onclick=\"richiediSuggerimento("+partita+")\" class=\"btn btn-info btn-sm\"> Richiedi Suggerimento</button></div></div>");
 		}
 		else{
 		    $("#inputModificaQuota"+partita).remove();	
@@ -49,6 +80,7 @@ function abilitaEsito(esito){
 	}
 	
 	function richiediSuggerimento(partita){
+
 		var esito=$("#inputModificaQuota"+partita+" "+'[name="'+"esitoScelto"+'"]').val();
 		$.ajax({
 			  type: 'POST',
@@ -62,4 +94,9 @@ function abilitaEsito(esito){
 
 			  }
 			});
+	}
+	
+	function controllaEsito(){
+		
+		
 	}
