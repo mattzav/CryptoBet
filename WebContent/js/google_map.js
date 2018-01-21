35eda311da1bf2a25d736c169c955dddb0831f01
mattzav <matteozav@hotmail.com>
2018-01-21 12:27:59 +0100

    pulizia pagine, glossario

--- WebContent/js/google_map.js
+++ WebContent/js/google_map.js
@@ -5,10 +5,8 @@ function init() {
 
 	var mapOptions = {
 		zoom : 10,
-		// The latitude and longitude to center the map (always required)
 		center : myLatlng,
 
-		// How you would like to style the map.
 		scrollwheel : false,
 		styles : [ {
 			"featureType" : "administrative.land_parcel",
@@ -91,15 +89,11 @@ function init() {
 		} ]
 	};
 
-	// Get the HTML DOM element that will contain your map
-	// We are using a div with id="map" seen below in the <body>
 	var mapElement = document.getElementById('map');
 
-	// Create the Google Map using out element and options defined above
 	var map = new google.maps.Map(mapElement, mapOptions);
 
 	var input = (document.getElementById('pac-input'));
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	var searchBox = new google.maps.places.SearchBox((input));
 	
@@ -188,6 +182,7 @@ function calcolaPercorso(pos) {
 		map : map
 	});
 
+	
 	// get route from A to
 	calculateAndDisplayRoute(directionsService, directionsDisplay, pointA,
 			pointB);
